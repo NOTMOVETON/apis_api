3954e6b9f091610d6700bc9a014124c531848d36
kdd40 <kd@newproductslab.ru>
2023-09-22 22:11:30 +0300

    123

--- main.cpp
+++ main.cpp
@@ -24,46 +24,47 @@ static bool isFloatNumber(const std::string& string){
     }
     return string.size()>minSize && it == string.end();
   }
-int main(){
-	std::vector<std::vector<long double>> message(8, std::vector<long double>(12));
-	int i=0, j=0;
-	for(int i=0; i<5; i++){
-		for(int j=0; j<12; j++){
-			message[i][j]=0.0;
-		}
-	}
+  // TODO: REMAKE!
+// int main(){
+// 	std::vector<std::vector<long double>> message(8, std::vector<long double>(12));
+// 	int i=0, j=0;
+// 	for(int i=0; i<5; i++){
+// 		for(int j=0; j<12; j++){
+// 			message[i][j]=0.0;
+// 		}
+// 	}
 
 
-	std::ifstream myfile;
-	myfile.open("C:\\Users\\dimak\\Downloads\\test2.waypoints");
+// 	std::ifstream myfile;
+// 	myfile.open("C:\\Users\\dimak\\Downloads\\test2.waypoints");
 
-	std::string myline;
-	if ( myfile.is_open() ) {
-		while (std::getline (myfile, myline)) {
-			j=0;
+// 	std::string myline;
+// 	if ( myfile.is_open() ) {
+// 		while (std::getline (myfile, myline)) {
+// 			j=0;
 			
-			std::cout.flush();
-			std::stringstream ss(myline);  
-    		std::string word;
-			while (ss >> word) { // Extract word from the stream.
-        		if(isFloatNumber(word)){
-					message[i][j]=std::stod(word);
-				}
-				std::cout.flush();
-				// std::cout<<i<<" "<<message[i][j]<<" "<<myfile.good()<<std::endl;
-				j++;
+// 			std::cout.flush();
+// 			std::stringstream ss(myline);  
+//     		std::string word;
+// 			while (ss >> word) { // Extract word from the stream.
+//         		if(isFloatNumber(word)){
+// 					message[i][j]=std::stod(word);
+// 				}
+// 				std::cout.flush();
+// 				// std::cout<<i<<" "<<message[i][j]<<" "<<myfile.good()<<std::endl;
+// 				j++;
 				
-    		}
-			i++;
-		}
-	}
-	myfile.close();
-	for(int k1=0; k1<8; k1++){
-		for(int k2=0; k2<12; k2++){
-			std::cout<<std::fixed << std::setprecision(6)<<message[k1][k2]<<' ';
-		}
-		std::cout<<'\n';
-	}
+//     		}
+// 			i++;
+// 		}
+// 	}
+// 	myfile.close();
+// 	for(int k1=0; k1<8; k1++){
+// 		for(int k2=0; k2<12; k2++){
+// 			std::cout<<std::fixed << std::setprecision(6)<<message[k1][k2]<<' ';
+// 		}
+// 		std::cout<<'\n';
+// 	}
 
 	return 0;
 }
